41083188566a6aa3be50537f295502baca9a93c6
Bennet Steem <bennet.steem@gmail.com>
2026-02-16 00:16:28 -0700

    Feb 16, 2026

--- greatest_common_divisor_of_strings.py
+++ greatest_common_divisor_of_strings.py
@@ -1,23 +1,7 @@
-def gcdOfStrings(self, str1: str, str2: str) -> str:
-    first_char = str1[0]
-    div = ""
-    max_str = str1 if len(str1) > len(str2) else str2
-    min_str = str1 if len(str1) <= len(str2) else str2
-    divisor_found = False
-
-    for i in range(len(max_str)):
-        min_idx = (i % len(min_str)) - 1    # convert to zero based index
-        div_idx = -1 if len(div) == 0 else (i % len(div)) - 1
-        if max_str[i] != min_str[min_idx]:
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
             return ""
 
-        elif divisor_found and div_idx > -1 and max_str[i] != div[div_idx]:    # we already know min_str == max_str
-            return ""
-
-        elif max_str[i] == first_char:
-            divisor_found = True
-
-        else:
-            div += max_str[i]
-
-    return div
+        max_len = gcd(len(str1), len(str2))
+        return str1[:max_len]
